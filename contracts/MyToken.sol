b9c7ea6f16f120f236dd06e5356c951907e6a172
yuk1ch1 <sma2ww@gmail.com>
2022-06-19 17:28:27 +0900

    impl: mintとburnを実装

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract MyToken is ERC20 {
+contract MyToken is ERC20, Ownable {
     // トークン名
     string tokenName;
     // シンボル名(i.e. btc, ethみたいなの)
@@ -18,7 +18,27 @@ contract MyToken is ERC20 {
     constructor(string memory _tokenName, string memory _tokenSymbol) ERC20(_tokenName, _tokenSymbol) {
         tokenName = _tokenName;
         tokenSymbol = _tokenSymbol;
-        _mint(msg.sender, 10000);
     }
 
+    /// トークンを発行するメソッド
+    ///    * @param to 発行先アドレス
+    ///    * @param amount 発行数
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _mint(address to, uint256 amount) internal override {
+        super._mint(to, amount);
+    }
+
+    ///    * トークンを償却する関数
+    ///    * @param to 発行先アドレス
+    ///    * @param amount 発行数
+    function burn(address to, uint256 amount) public onlyOwner {
+        _burn(to, amount);
+    }
+
+    function _burn(address to, uint256 amount) internal override {
+        super._burn(to, amount);
+    }
 }
