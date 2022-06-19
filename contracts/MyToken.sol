7d1bbc0e3618c4f930e37e5f336293ccccceee3f
yuk1ch1 <sma2ww@gmail.com>
2022-06-19 16:19:51 +0900

    impl: 不要なファイルを削除し、MyToken.solを作成。ERC20に準拠させた

---
+++ contracts/MyToken.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract MyToken is ERC20 {
+    // トークン名
+    string tokenName;
+    // シンボル名(i.e. btc, ethみたいなの)
+    string tokenSymbol;
+
+    constructor(string memory _tokenName, string memory _tokenSymbol) ERC20(_tokenName, _tokenSymbol) {
+        tokenName = _tokenName;
+        tokenSymbol = _tokenSymbol;
+    }
+
+}
